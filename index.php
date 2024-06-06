aa851ef73e6cd57109cdff6356e5f03900bedbf7
Wisnu K. Ardianto <setrasariputra@gmail.com>
2024-06-06 13:26:51 +0700

    array to string with serialize

--- index.php
+++ index.php
@@ -1,22 +1,29 @@
 <?php
-
-$plaintext = 'My password Login';
-$password = '123456789#abcdefghijklmnopqrstuvwxyz';
+$data = array(
+    'name' => 'Wisnu Ardianto',
+    'email' => 'setrasariputra@gmail.com',
+    'role' => array(
+        'name' => 'Super Admin',
+        'access' => 'Full Access'
+    ),
+);
+$plaintext = serialize($data);
+$phrase = '123456789#abcdefghijklmnopqrstuvwxyz';
 $method = 'aes-256-cbc';
 
 echo "Plaintext: ".$plaintext. "<br />";
 
 // must be exact 128 chars (512 bit)
-$password = hash('sha512', $password);
-echo "Password: ".$password. "<br />";
+$key = bin2hex(hash('sha512', $phrase));
+echo "Key: ".$key. "<br />";
 
 // IV must be exact 16 chars (128 bit)
 $iv = str_repeat(chr(0x0), 16);
 
 // start encrypt
-$encrypted = base64_encode(openssl_encrypt($plaintext, $method, $password, OPENSSL_RAW_DATA, $iv));
+$encrypted = base64_encode(openssl_encrypt($plaintext, $method, $key, OPENSSL_RAW_DATA, $iv));
 echo "Encrypted: ".$encrypted. "<br />";
 
 // start decrypt
-$decrypted = openssl_decrypt(base64_decode($encrypted), $method, $password, OPENSSL_RAW_DATA, $iv);
+$decrypted = openssl_decrypt(base64_decode($encrypted), $method, $key, OPENSSL_RAW_DATA, $iv);
 echo "Decrypted: ".$decrypted. "<br />";
